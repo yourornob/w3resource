8c6173c7da9ca5a4898cd9b97419e5d5f2a01074
Ornob <yourornob@gmail.com>
2022-03-02 14:03:50 +0600

    Ok

---
+++ Write a C program to compute the perimeter and area of a rectangle with a height of 7 inches. and width of 5 inches..c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d %d",&a,&b);
+    c=2*(a+b);
+    d=a*b;
+    printf("Perimeter of the rectangle = %d\n Area of the rectangle = %d\n",c ,d);
+    return 0;
+}
